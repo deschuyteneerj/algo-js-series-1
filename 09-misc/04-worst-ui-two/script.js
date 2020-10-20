395ba260c803fb16a91e498ddbc29e299c56dda8
Jonathan Deschuyteneer <deschuyteneerj@gmail.com>
2020-10-20 17:08:51 +0200

    final update last exercises 9.4 & 9.5

--- 09-misc/04-worst-ui-two/script.js
+++ 09-misc/04-worst-ui-two/script.js
@@ -1,7 +1,54 @@
 // 09-misc/04-worst-ui-two/script.js - 9.4: worst user interface (2) : phone clicker
 
 (() => {
+    let button1Value = parseInt(document.querySelectorAll("button")[0].getAttribute("data-min"));
+    let button2Value = parseInt(document.querySelectorAll("button")[1].getAttribute("data-min"));
+    let button3Value = parseInt(document.querySelectorAll("button")[2].getAttribute("data-min"));
+    let button4Value = parseInt(document.querySelectorAll("button")[3].getAttribute("data-min"));
 
-    // your code here
+    // Check if the digit is between 0 and 9 to add a 0 in front of the number -> 00 to 09:
+    function digitsBetween0And9(x) {
+        if (x <= 9) {
+            return "0" + x;
+        }
+        return x;
+    }
+    
+    // Event on button click:
+    document.querySelectorAll("button")[0].addEventListener("click", () => {
+        // Check if button value is under the data max required:
+        if (button1Value < parseInt(document.querySelectorAll("button")[0].getAttribute("data-max"))) {
+            // If condition is met:
+            button1Value++;
+            // Apply the function to check if the number is between 0 and 9 and display it:
+            document.querySelectorAll("button")[0].innerHTML = digitsBetween0And9(button1Value);
+            // Put the selected value in the displayed phone number above the buttons:
+            document.getElementById("target").innerHTML = `+${document.querySelectorAll("button")[0].innerHTML}${document.querySelectorAll("button")[1].innerHTML}${document.querySelectorAll("button")[2].innerHTML}${document.querySelectorAll("button")[3].innerHTML}`
+        }
+    });
 
+    // Comments for the first addEventListener are the same for the 3 others:
+    document.querySelectorAll("button")[1].addEventListener("click", () => {
+        if (button2Value < parseInt(document.querySelectorAll("button")[1].getAttribute("data-max"))) {
+            button2Value++;
+            document.querySelectorAll("button")[1].innerHTML = digitsBetween0And9(button2Value);
+            document.getElementById("target").innerHTML = `+${document.querySelectorAll("button")[0].innerHTML}${document.querySelectorAll("button")[1].innerHTML}${document.querySelectorAll("button")[2].innerHTML}${document.querySelectorAll("button")[3].innerHTML}`
+        }
+    });
+
+    document.querySelectorAll("button")[2].addEventListener("click", () => {
+        if (button3Value < parseInt(document.querySelectorAll("button")[2].getAttribute("data-max"))) {
+            button3Value++;
+            document.querySelectorAll("button")[2].innerHTML = digitsBetween0And9(button3Value);
+            document.getElementById("target").innerHTML = `+${document.querySelectorAll("button")[0].innerHTML}${document.querySelectorAll("button")[1].innerHTML}${document.querySelectorAll("button")[2].innerHTML}${document.querySelectorAll("button")[3].innerHTML}`
+        }
+    });
+
+    document.querySelectorAll("button")[3].addEventListener("click", () => {
+        if (button4Value < parseInt(document.querySelectorAll("button")[3].getAttribute("data-max"))) {
+            button4Value++;
+            document.querySelectorAll("button")[3].innerHTML = digitsBetween0And9(button4Value);
+            document.getElementById("target").innerHTML = `+${document.querySelectorAll("button")[0].innerHTML}${document.querySelectorAll("button")[1].innerHTML}${document.querySelectorAll("button")[2].innerHTML}${document.querySelectorAll("button")[3].innerHTML}`
+        }
+    });
 })();
--- 09-misc/05-worst-ui-three/script.js
+++ 09-misc/05-worst-ui-three/script.js
@@ -1,7 +1,31 @@
 // 09-misc/05-worst-ui-three/script.js - 9.5: worst user interface (3) : phone slot
 
 (() => {
+    const inputs = document.querySelectorAll("input");
 
-    // your code here
+    // Generate a number between the mix and max values from the input:
+    function randInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
 
+    // Check if the digit is between 0 and 9 to add a 0 in front of the number -> 00 to 09:
+    function digitsBetween0And9(x) {
+        // To ensure the number is an integer not a string:
+        x = parseInt(x);
+        if (x <= 9) {
+            return "0" + x;
+        }
+        return x;
+    }
+
+    // For each "input" button:
+    document.querySelectorAll("input").forEach((input) => {
+        // Event on button click:
+        input.parentElement.querySelector("button").addEventListener("click", () => {
+            // Generate the random input to display:
+            input.value = randInt(parseInt(input.getAttribute("data-min")), parseInt(input.getAttribute("data-max")));
+            // Display the random input into the target:
+            document.getElementById("target").innerHTML = `+${inputs[0].value}${digitsBetween0And9(inputs[1].value)}${digitsBetween0And9(inputs[2].value)}${digitsBetween0And9(inputs[3].value)}`
+        });
+    });
 })();
